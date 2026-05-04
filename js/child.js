01e77f72725f0c8d1b63f00c760316daaf0f60c3
Tamim Rahman Khan <tamimkhanbdworks@gmail.com>
2026-05-04 12:24:39 +0600

    Updated

---
+++ js/child.js
@@ -0,0 +1,11 @@
+
+ const newChild = document.createElement('li');
+
+ newChild.innerText = 'New Born footballer here';
+
+ const playersList = document.getElementById('player-list');
+
+ playersList.appendChild(newChild);
+
+
+
